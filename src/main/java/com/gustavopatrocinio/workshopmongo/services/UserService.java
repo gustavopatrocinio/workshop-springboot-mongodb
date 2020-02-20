89eff5824e874771475ed5c1a794e5420177f351
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2020-02-20 15:56:36 -0300

    Conectando ao mongoDB com repository e service

---
+++ src/main/java/com/gustavopatrocinio/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.gustavopatrocinio.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavopatrocinio.workshopmongo.domain.User;
+import com.gustavopatrocinio.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll(){
+		return repo.findAll();
+		
+	}
+}
