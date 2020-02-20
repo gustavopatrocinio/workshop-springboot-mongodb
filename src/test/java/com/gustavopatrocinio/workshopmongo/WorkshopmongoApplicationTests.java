2c38bb41a5423657d6d142bcd277d377fc985084
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2020-02-20 12:12:58 -0300

    Projeto criado

---
+++ src/test/java/com/gustavopatrocinio/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavopatrocinio.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
