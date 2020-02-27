092f179d077b7ed3154745ca89c63e2baabe0b4c
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2020-02-27 09:16:29 -0300

    Obtendo post por id

---
+++ src/main/java/com/gustavopatrocinio/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.gustavopatrocinio.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavopatrocinio.workshopmongo.domain.Post;
+import com.gustavopatrocinio.workshopmongo.repository.PostRepository;
+import com.gustavopatrocinio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
